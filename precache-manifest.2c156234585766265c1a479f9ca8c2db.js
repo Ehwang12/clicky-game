18edc04bd978b981aaaccdaccf590405d8ca3a53
Esther <estshwang@gmail.com>
2019-06-21 23:58:25 -0500

    Updates

---
+++ precache-manifest.2c156234585766265c1a479f9ca8c2db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd97ddcb6c2834a6d08c5923435babf",
+    "url": "/Ehwang12/clicky-game/index.html"
+  },
+  {
+    "revision": "2e4456e690bc773c0f23",
+    "url": "/Ehwang12/clicky-game/static/css/main.a61e13a8.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Ehwang12/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "2e4456e690bc773c0f23",
+    "url": "/Ehwang12/clicky-game/static/js/main.eb3ae98b.chunk.js"
+  },
+  {
+    "revision": "4bf3e8c9caa1a9e648f2",
+    "url": "/Ehwang12/clicky-game/static/js/runtime~main.5b78adb5.js"
+  }
+]);
